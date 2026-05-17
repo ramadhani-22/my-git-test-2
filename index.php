acf7b3e39ada3d1e0e2bf1f82e7b881135320004
ramadhani-22 <aramadhani2212@gmail.com>
2026-05-17 12:50:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
         Cupiditate odit corrupti quae atque deserunt pariatur quos repellendus explicabo, accusamus placeat illum reiciendis voluptatibus, 
-        unde aliquid qui, nulla ipsum sequi consectetur.</p>
+        unde aliquid qui, nulla ipsum sequi consectetur.
+    </p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
